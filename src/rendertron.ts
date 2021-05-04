392a2411bf2f5e1e2f8f07a79e130544e8f2fb70
Olivia <oliviadunnett4@gmail.com>
2021-05-04 10:32:49 +0100

    30 max browsers

--- src/rendertron.ts
+++ src/rendertron.ts
@@ -32,7 +32,7 @@ export class Rendertron {
       }
     }, {
       min: 2,
-      max: 50,
+      max: 30,
       autostart: true
     })
     // const browser = await puppeteer.launch({ args: config.puppeteerArgs });
