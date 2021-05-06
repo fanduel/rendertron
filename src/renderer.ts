c3eed4bd7a7ea01e770b4d11af614c5fb5a79940
Olivia <oliviadunnett4@gmail.com>
2021-05-06 10:26:04 +0100

    network idle 2

--- src/renderer.ts
+++ src/renderer.ts
@@ -285,7 +285,7 @@ export class Renderer {
       // Navigate to page. Wait until there are no oustanding network requests.
       response = await page.goto(url, {
         timeout: this.config.timeout,
-        waitUntil: 'networkidle0',
+        waitUntil: 'networkidle2',
       });
     } catch (e) {
       console.error(e);
