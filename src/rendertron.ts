54b5ea2e425f17136e31822f4a79545110d53331
Olivia <oliviadunnett4@gmail.com>
2021-05-19 13:06:46 +0100

    timeout back in

--- src/rendertron.ts
+++ src/rendertron.ts
@@ -37,6 +37,7 @@ export class Rendertron {
     const cluster = await Cluster.launch({
       concurrency: Cluster.CONCURRENCY_CONTEXT,
       maxConcurrency: 50,
+      timeout: config.timeout * 4,
       puppeteerOptions: { args: config.puppeteerArgs },
     });
     this.cluster = cluster;
